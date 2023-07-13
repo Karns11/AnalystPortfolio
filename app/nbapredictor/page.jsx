a293af4f5eff541598b05a23eef626aec0d368eb
Nathan karns <107375013+Karns11@users.noreply.github.com>
2023-07-13 11:54:33 -0400

    Update page.jsx

--- app/nbapredictor/page.jsx
+++ app/nbapredictor/page.jsx
@@ -8,7 +8,18 @@ import headers from "@public/assets/projects/headers.png";
 import datesandoct from "@public/assets/projects/datesandoct.png";
 import novanddec from "@public/assets/projects/novanddec.png";
 import df1 from "@public/assets/projects/1df.png";
-import heatMap from "@public/assets/projects/heatMap.png";
+import teamstatslogic from "@public/assets/projects/teamstatslogic.png";
+import teamstatsdf from "@public/assets/projects/teamstatsdf.png";
+import oppteamstatslog from "@public/assets/projects/oppteamstatslog.png";
+import oppteamstatsdf from "@public/assets/projects/oppteamstatsdf.png";
+import combineddf from "@public/assets/projects/combineeddf.png";
+import createnewdf from "@public/assets/projects/createnewdf.png";
+import newdf from "@public/assets/projects/newdf.png";
+import linreg from "@public/assets/projects/linreg.png";
+import svcscore from "@public/assets/projects/svcscore.png";
+import meansquarederror from "@public/assets/projects/meansquarederror.png";
+import comparisondf from "@public/assets/projects/comparisondf.png";
+import overallcorrectperc from "@public/assets/projects/overallcorrectperc.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
@@ -131,9 +142,151 @@ const property = () => {
             alt="/" />  
 
           <p className="mt-4">
-            I am currently working on updating this blog post. Please refer to my github repo if you wish to see 
-            the remainder of the code.
+            Next, I need to get the team stats per game in order to make the model more accurate. These stats will include 
+            things like fg%, ft%, 3pt%, ast, stl, and a whole lot more. For this, we need to use a new url so we will 
+            reininitalize our response variable as well as our soup variable. I will then look through all the tr tags and 
+            grab the th tags that are in the 72nd index because that index contains the table that we want. We dont want the headers
+            so we will skip the 0 index by saying total_stats = total_stats[1:]. I will do the same process for both the total stats 
+            and per 100 game stats which are tables on the same url web page. I may or may not use these two varibles later on. <br /> <br />
+            Next, I need to get the name of each team and put that at the 0 index of each list so that it looks the the_data variable 
+            looks something like this [[team, stat1, stat2, ...] [team, stat1, stat2,...]...]. After I did this, I realized that some 
+            of the team names had a random '*' at the end. So, I had to make a new varible called clean_data and loop through the list 
+            I just created and use rstrip to remove that '*' from the end and then append the new team list to the clean_data variable. <br /> <br />
+            Now, I have what I need to create a 2023 team per game stats df.
           </p>
+
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={teamstatslogic}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Here is what that should look like: 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={teamstatsdf}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Next, I want to know what each teams opposing team stats are per game. Basically, I want to know how good or bad 
+            each team is defensively. Here, I will basicall do the same thing I just did to create an opposing teams stats df.
+            I will have to clean the data just like above because I will still getting that random annoying '*' at the end of 
+            some team names.
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={oppteamstatslog}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Here is what that should look like: 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={oppteamstatsdf}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Now, it is time to combine the previous two df's into one big df. This will easily show us all the relevant 
+            stats per team. This is what it should look like: 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={combineddf}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Finally, the last thing we need to do before predictions, is to combine the big df we just made with the 
+            game results df. This will give us the scores for each game as well as the teams that played and the stats going 
+            into each game for each team. Here is how I did that: 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={createnewdf}
+            alt="/" /> 
+          
+          <p className="mt-4">
+            Here's what that df looks like: 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={newdf}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Next, lets import what we need to conduct linear regression and see how well this model fits our data. Here, I will 
+            create the train and test variables that will be used with other models as well.
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={linreg}
+            alt="/" /> 
+          <p className="mt-4">
+            This gives us a really bad score so let's move onto a different model.
+          </p>
+          <p className="mt-4">
+            I will speed this us and just tell you that I found the SVC model to be the best. Here is how I found that out:
+            <br /> <br /> Although .60 isn't the best score, it is good enough for me to use for this analysis. 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={svcscore}
+            alt="/" /> 
+          <p className="mt-4">
+            Next, I wanted to find the mean sqaured error for this model in order to see how accurate it is. The lower the 
+            score the better, so 0.24 is going to be acceptable for this project.
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={meansquarederror}
+            alt="/" /> 
+
+          <p className="mt-4">
+            Next, I wanted to actually look at the games it predicted and see how many it got correct. I will create a quick 
+            comparison df for this.
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={comparisondf}
+            alt="/" /> 
+
+          <p className="mt-4">
+            This is kind of hard to see, so I will run ana analysis on this df and come up with an overall % correct. Here 
+            is how I did that: 
+          </p>
+          <Image 
+            className="hover:scale-105 ease-in duration-300 shadow-lg shadow-gray-400 mt-4"
+            width={800}
+            height={500}
+            src={overallcorrectperc}
+            alt="/" /> 
+          <p className="mt-4">This shows us that the model has greater than a 63% success rate! This is honestly really good 
+          in my opinion because this is the first model I have ever created. I expect that this model will be even more accurate 
+          once I include data from the remainder of the season, after december. <br /> <br /> All in all, I think this project was 
+          a success.</p>
 
 
           <div className="flex justify-center">
